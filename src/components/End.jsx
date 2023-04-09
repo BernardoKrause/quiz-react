040aaf754c13a1003b1b609b6cda5359c1b592a9
Bernardo Krause <bernardokrause@hotmail.com>
2023-04-09 17:02:06 -0300

    ajuste básico de css

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './End.css'
 
 const End = ( {changeGameStage} ) => {
   return (
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Start.css'
 
 const Start = ( {changeGameStage} ) => {
   return (
